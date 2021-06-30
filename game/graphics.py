84da6f6a2b2274c1c8292ad60bb0bc33e06c36a8
bourbonut <ben.bourbon06@gmail.com>
2021-06-30 15:44:44 +0200

    Updating the game 2048 (animations and images)

--- game/graphics.py
+++ game/graphics.py
@@ -1,92 +1,264 @@
 # Interface du jeu
 
 import pygame
+from copy import copy
+from pygame.math import Vector2 as vec2
 from pathlib import Path
+import yaml
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+colors = yaml.load(stream=open("colors.yaml", "r"), Loader=Loader)
+
+
+def sprite(number: int) -> pygame.Surface:
+    """
+    Generate a surface with the right color and the number written
+    """
+    color, size, font_color = colors[number]
+    sprite = pygame.Surface((107, 107), pygame.SRCALPHA)
+    sprite.convert_alpha()
+    pygame.draw.rect(sprite, color, pygame.Rect(0, 0, 107, 107), border_radius=5)
+    font = pygame.font.Font("clear-sans.bold.ttf", size)
+    text = font.render(f"{number}", True, font_color)
+    w, h = text.get_rect().center
+    sprite.blit(text, ((53 - w, 53 - h - 5)))
+    return sprite
+
+
+def background(locations: list) -> pygame.Surface:
+    """
+    Generate the background of the game
+    """
+    background = pygame.Surface((500, 500))
+    background.fill(colors[-1][0])
+    color = colors[0][0]
+    for location in locations:
+        rect = pygame.Rect(location, (107, 107))
+        pygame.draw.rect(background, color, rect, border_radius=5)
+    return background
+
+
+def gameover() -> pygame.Surface:
+    """
+    Generate the gameover surface
+    """
+    gameover = pygame.Surface((500, 500))
+    gameover.set_alpha(120)
+    font = pygame.font.Font("clear-sans.bold.ttf", 56)
+    text = font.render("Game over", True, [255, 0, 0])
+    w, h = text.get_rect().center
+    gameover.blit(text, ((250 - w, 250 - h - 5)))
+    return gameover
 
 
 class Interface:
 
     """
-    Classe Interface du jeu 2048
+    Graphic interface for 2048 game
     """
 
-    PATH = Path(__file__).parents[1].absolute() / "images"
+    NB_I = 20  # number of how many images will be drawn for an animation
+    NB_H = 20  # number of how many homotheties will be applied for a new number after an addition
+    FPS = 240  # Frames per second
 
     def __init__(self):
-        # Initialise les directions pour l'animation
-        self.directions = {
-            1: self.vectors((-100, 0), lambda p: (p, 0)),  # Droite
-            -1: self.vectors((100, 0), lambda p: (-p, 0)),  # Gauche
-            4: self.vectors((0, -100), lambda p: (0, p)),  # Bas
-            -4: self.vectors((0, 100), lambda p: (0, -p)),  # Haut
-        }
-
-        # Liste des couples de points pour les emplacements d'images
-        self.emplacements = [
-            (15 + 121 * ligne, 15 + 121 * colonne)
+        # List of vectors for locations of images
+        self.locations = [
+            vec2(15 + 121 * ligne, 15 + 121 * colonne)
             for colonne in range(4)
             for ligne in range(4)
         ]
 
-        # Initialisation de l'afficheur
-        pygame.display.init()
-        taille = longueur, hauteur = (500, 500)
-        # Initialisation de la fenetre
-        self.win = pygame.display.set_mode(taille)
+        pygame.init()
+        self.win = pygame.display.set_mode((500, 500))
         pygame.display.set_caption("2048")
 
-        # Ouverture de toutes les images
-        load_image = lambda e: pygame.image.load(str(self.PATH / e)).convert_alpha()
-        self.background = load_image("background.png")
-        self.image_game_over = load_image("game_over.png")
-        self.images = {2 ** i: load_image(f"number{2**i}.png") for i in range(1, 12)}
-        self.images[0] = load_image(f"number0.png")
+        # Draw all images
+        self.background = background(self.locations)
+        self.images = {0: sprite(0)}
+        self.images.update({2 ** i: sprite(2 ** i) for i in range(1, 18)})
+        self.gameover = gameover()
 
-        # Set du background
+        # Set background
         self.win.blit(self.background, (0, 0))
         self.flip()
 
-    def vectors(self, translation_vector: tuple, zero: tuple) -> callable:
-        """Genere les fonctions utiles pour les directions"""
-        translation = lambda v: (
-            v[0] + translation_vector[0],
-            v[1] + translation_vector[1],
-        )
-        return lambda v, p: (translation(v), zero(p))
+        # Some useful parameters
+        self.moves, self.additions = [], []
+        self.before, self.after = [], []
+        self.clock = pygame.time.Clock()
 
     def blit(self, n_image: int, position: int) -> None:
-        """Dessine l'image suivant l'emplacement"""
-        self.win.blit(self.images[n_image], self.emplacements[position])
+        """Draw the image `n_image` according to the `position`"""
+        self.win.blit(self.images[n_image], self.locations[position])
 
     def update(self, position: int) -> None:
-        """Met a jour une partie de l'ecran"""
-        coords = self.emplacements[position]
+        """Update a part of the screen"""
+        coords = self.locations[position]
         pygame.display.update(pygame.Rect(coords[0], coords[1], 107, 107))
 
     def flip(self):
-        """Met a jour tout l'ecran"""
+        """Update all the screen"""
         pygame.display.flip()
 
     def game_over(self):
-        """Affiche l'ecran de fin du jeu (game over)"""
-        self.win.blit(self.image_game_over, (0, 0))
+        """Display the game over image on the screen"""
+        self.win.blit(self.gameover, (0, 0))
         self.flip()
 
-    def animation(self, image: int, position: int, direction: int) -> None:
-        """direction = Case suivante - Case précédente"""
-        # d comme départ et c comme coeffient
-        translation = lambda d, c: (d[0] + c[0], d[1] + c[1])
-        depart = self.emplacements[position - direction]
-        arrivee = self.emplacements[position]
-        param = 10  # parametre de fluidite (plus petit, plus animation fluide)
-        coords, vecteur = self.directions[direction](arrivee, param)
-        self.win.blit(self.background, (0, 0))
-        pygame.display.update(pygame.Rect(depart[0], depart[1], 107, 107))
-        for i in range(100 // param):
-            self.win.blit(self.images[image], coords)
-            pygame.display.update(pygame.Rect(coords[0], coords[1], 107, 107))
+    def update_moves(self, directions):
+        for direction in directions:
+            before = [self.before[i] for i in direction]
+            after = [self.after[i] for i in direction]
+            self.moving(before, after, direction)
+
+    def moving(self, before: list, after: list, direction: list) -> None:
+        """Find moves and additions for animations and store them in `moves` and `additions`"""
+        i = 3
+        while i > -1:
+            if not after[i]:
+                break
+            elif after[i] == before[i]:
+                i -= 1
+                continue
+            reference, j = after[i], i
+            while j > -1:
+                item = before[j]
+                if item == reference:
+                    self.moves.append([direction[j], direction[i], before[j]])
+                    break
+
+                elif item and item < reference:
+                    k, found = 1, False
+                    while k < j + 1:
+                        if before[j - k] and before[j - k] != item:
+                            break
+                        elif before[j - k] == item:
+                            self.additions.append([direction[i], reference])
+                            if i != j:
+                                self.moves.append([direction[j], direction[i], item])
+                            self.moves.append([direction[j - k], direction[i], item])
+                            before = before[: j - k] + [0] * (4 - j + k)
+                            found = True
+                            break
+                        k += 1
+                    if found:
+                        break
+                j -= 1
+            i -= 1
+
+    def reset_animation(self):
+        """Reset animation lists"""
+        self.moves = []
+        self.additions = []
+
+    def setbefore(self, before: list):
+        """Set `before` list"""
+        self.before = copy(before)
+
+    def setafter(self, after: list):
+        """Set `after` list"""
+        self.after = copy(after)
+
+    def allmovements(self):
+        """
+        Generate a list `movements` which, for all movements, contains:
+            - the image of the number which will move
+            - the starting position of the number
+            - `q` which is the euclidian quotient of `(end - start) // nb_images`
+            - `r` which is the euclidian remainder of `(end - start) % nb_images`
+            - `symbol` which is 1 if end > start else -1
+
+        Notes:
+            `pygame` offers the class `Vector2` which in my opinion, is not complete.
+            You can try euclidian division but it will not return the expected result.
+            For instance:
+            ```
+            >>> (end - start)//20
+            <Vector2(-13, 0)>
+            >>> (378-136)//20
+            12
+            ```
+            In addition, the class doesn't support the operator `%`.
+        """
+        self.movements = []
+        for start, end, number in self.moves:
+            symbol = 1 if end > start else -1
+            start = self.locations[start]
+            end = self.locations[end]
+            diff = end - start
+            q = vec2(diff[0] // self.NB_I, diff[1] // self.NB_I)
+            r = vec2(diff[0] % self.NB_I, diff[1] % self.NB_I)
+            r = r / self.NB_I
+            limits = vec2((107, 107)) + 2 * symbol * q
+            data = (start, limits, q, r, symbol)
+            self.movements.append([self.images[number], data])
+
+    def allscales(self):
+        """
+        Generate a dictionary `allscales` where:
+            - the key is the number
+            - the value is a list of a succession of images of the number
+              with different font size
+        Note:
+            The first element of each list is the number with a font size equal to 0,
+            so without text.
+        """
+        self.scales = {}
+        for number in {number for _, number in self.additions}:
+            color, size, font_color = colors[number]
+            images = []
+            q, r = size // self.NB_H, (size % self.NB_H) / self.NB_H
+
+            for s in range(self.NB_H + 1):
+                image = pygame.Surface((107, 107), pygame.SRCALPHA)
+                image.convert_alpha()
+                region = pygame.Rect(0, 0, 107, 107)
+                pygame.draw.rect(image, color, region, border_radius=5)
+                font = pygame.font.Font("clear-sans.bold.ttf", s * q + int(s * r))
+                text = font.render(f"{number}", True, font_color)
+                w, h = text.get_rect().center
+                image.blit(text, ((53 - w, 53 - h - 5)))
+                images.append(image)
+            self.scales[number] = images
+
+    def animation(self, directions: list) -> None:
+        """
+        Manage all animations of the game
+        """
+        self.update_moves(directions)
+        self.allscales()
+        self.allmovements()
+        for i in range(self.NB_I + 1):
+            self.clock.tick(self.FPS)
             self.win.blit(self.background, (0, 0))
-            # parametre de vitesse (plus grand, plus vitesse lente)
-            pygame.time.delay(4)
-            pygame.display.update(pygame.Rect(coords[0], coords[1], 107, 107))
-            coordonnees = translation(coords, vecteur)
+            regions = []
+
+            for images, data in self.movements:
+                start, limits, q, r, symbol = data
+                vec = start + i * q + vec2(int(i * r[0]), int(i * r[1]))
+                self.win.blit(images, vec)
+                k = (i - 1) if symbol > 0 else i
+                vec = start + k * q + vec2(int(k * r[0]), int(k * r[1]))
+                regions.append(pygame.Rect(vec, limits))
+
+            for pos, number in self.additions:
+                image = self.scales[number][0]
+                self.win.blit(image, self.locations[pos])
+                regions.append(pygame.Rect(self.locations[pos], (107, 107)))
+
+            pygame.display.update(regions)
+
+        for i in range(0, self.NB_I + 1):
+            regions = []
+            for pos, number in self.additions:
+                image = self.scales[number][i]
+                self.win.blit(image, self.locations[pos])
+                regions.append(pygame.Rect(self.locations[pos], (107, 107)))
+            pygame.display.update(regions)
+
+        self.reset_animation()
--- game/program2048.py
+++ game/program2048.py
@@ -12,66 +12,56 @@ import os
 class Game2048:
 
     """
-    Jeu generique 2048
+    Generic 2048 game
     """
 
-    LIGNE = {0: [0, 1, 2, 3], 1: [4, 5, 6, 7], 2: [8, 9, 10, 11], 3: [12, 13, 14, 15]}
-    INV_LIGNE = {
-        0: [3, 2, 1, 0],
-        1: [7, 6, 5, 4],
-        2: [11, 10, 9, 8],
-        3: [15, 14, 13, 12],
-    }
-    COLONNE = {0: [0, 4, 8, 12], 1: [1, 5, 9, 13], 2: [2, 6, 10, 14], 3: [3, 7, 11, 15]}
-    INV_COLONNE = {
-        0: [12, 8, 4, 0],
-        1: [13, 9, 5, 1],
-        2: [14, 10, 6, 2],
-        3: [15, 11, 7, 3],
+    ORDERS = {
+        1: [[i for i in range(k * 4, k * 4 + 4)] for k in range(4)],
+        -1: [[i for i in range(k * 4 + 4 - 1, k * 4 - 1, -1)] for k in range(4)],
+        4: [[k + 4 * i for i in range(4)] for k in range(4)],
+        -4: [[k + 4 * i for i in range(4 - 1, -1, -1)] for k in range(4)],
     }
 
     def __init__(self):
-        self.grille = [0 for i in range(16)]
+        self.grid = [0] * 16
         self.zero = list(range(16))
         self.score = 0
-        self.movements = {
-            -4: self.movement_condition(self.LIGNE[0], -4),  # Haut
-            4: self.movement_condition(self.LIGNE[3], 4),  # Bas
-            -1: self.movement_condition(self.COLONNE[0], -1),  # Gauche
-            1: self.movement_condition(self.COLONNE[3], 1),  # Droite
-        }
-        self.time = 60
+
+        # actionkeys and vs (order): [up, down, left, right]
+        self.actionkeys = (-4, 4, -1, 1)
+        vs = ([0, 1, 2, 3], [12, 13, 14, 15], [0, 4, 8, 12], [3, 7, 11, 15])  # values
+        self.movements = {k: self.condition(v, k) for k, v in zip(self.actionkeys, vs)}
 
     def __len__(self):
-        return len(self.grille)
+        return len(self.grid)
 
     def __getitem__(self, key):
-        return self.grille[key]
+        return self.grid[key]
 
     def __setitem__(self, key, item):
-        self.grille[key] = item
+        self.grid[key] = item
 
     def __iadd__(self, item):
-        """Uniquement pour la liste des zéros"""
+        """Only for zeroes list"""
         self.zero.append(item)
         return self
 
     def __isub__(self, item):
-        """Uniquement pour la liste des zéros"""
+        """Only for zeroes list"""
         self.zero.remove(item)
         return self
 
     def __repr__(self):
-        return "\n".join((str(self[4 * i : 4 * (i + 1)]) for i in range(4)))
+        return "\n".join((str(self[4 * i : 4 * (i + 1)]) for i in range(4))) + "\n"
 
     def restart(self) -> None:
-        """Recommence le jeu"""
-        self.grille = [0 for i in range(16)]
+        """Restart game"""
+        self.grid = [0] * 16
         self.zero = list(range(16))
         self.score = 0
 
     def set_first_elements(self) -> None:
-        """Initialise les premiers elements de jeu"""
+        """Initialize the first elements of the game"""
         a, b, c = randint(0, 15), randint(0, 15), randint(0, 15)
         self[a], self[b], self[c] = (self.random_2_4() for _ in range(3))
         self -= a
@@ -81,34 +71,32 @@ class Game2048:
             self -= c
         if self.graphism:
             self.interface = Interface()
-            for i in range(16):
-                self.update(i)
+            self.update_all()
 
     def generic_start(self, function: callable, ending: bool = False) -> None:
         """
-        Permet de jouer selon la fonction 'function' avec l'interface graphique.
-        Si 'ending' faux, le jeu se ferme directement apres le game over
+        Allow to play according to the argument `function` with the graphic interface
+        If `ending` is `False`, the game stops directly after a game over
         """
         self.graphism = True
         self.set_first_elements()
         self.run = True
         while self.move() and self.run:
             while self.zero and self.run:
-                self.is_running()
+                self.isrunning()
                 function()
-            self.is_running()
+            self.isrunning()
             function()
         if self.run:
-            for i in range(16):
-                self.update(i)
+            self.update_all()
             self.interface.game_over()
             if ending:
                 while self.run:
-                    self.is_running()
+                    self.isrunning()
         pygame.quit()
 
     def generic_ai_start(self, function: callable) -> None:
-        """Permet de jouer selon la fonction 'function' sans l'interface graphique"""
+        """Allow to play according to the argument `function` without the graphic interface"""
         self.graphism = False
         self.set_first_elements()
         while self.move():
@@ -116,158 +104,114 @@ class Game2048:
                 function()
             function()
 
-    def is_running(self) -> None:
-        """Code pour pouvoir fermer la fenetre"""
+    def isrunning(self) -> None:
+        """Code to be able to close the window"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.run = False
 
     def random_2_4(self) -> int:
-        """Genere aleatoirement un 2 (probabilite:0.8) ou un 4 (probabilite:0.2)"""
+        """Generate randomly 2 (probability: 0.8) or 4 (probability: 0.2)"""
         return 2 if random() <= 0.8 else 4
 
-    def move_zero(self, liste: list) -> None:
-        """Cherche le premier zero puis lance 'moving'"""
-        index = 3
-        arrive = 0
-        while self[liste[arrive]] != 0 and index > -1:
-            arrive = index
-            index -= 1
-        if index == -1:
-            return
-        else:
-            self.moving(0, arrive, liste)
-
-    def moving(self, depart: int, arrive: int, liste: list) -> None:
-        """Deplace les zeros et les nombres normaux"""
-        suivant = depart + 1
-        if suivant > 3:
-            return
-        elif self[liste[suivant]] != 0:
-            self.moving(suivant, arrive, liste)
-        elif self[liste[suivant]] == self[liste[depart]]:
-            self.moving(suivant, arrive, liste)
-        else:
-            self[liste[depart]], self[liste[suivant]] = (
-                self[liste[suivant]],
-                self[liste[depart]],
-            )
-            self -= liste[suivant]
-            self += liste[depart]
-            if self.graphism:
-                self.animation(liste[suivant], liste[depart])
-            if depart > 0 and self[liste[depart - 1]] != 0:
-                self.moving(depart - 1, arrive, liste)
+    def move_zero(self, order: list) -> None:
+        """Find the first zero and start 'moving'"""
+        for list_ in order:
+            index = 3
+            end = 0
+            while self[list_[end]] != 0 and index > -1:
+                end = index
+                index -= 1
+            if index == -1:
+                continue
             else:
-                self.moving(suivant, arrive, liste)
+                self.moving(0, end, list_)
 
-    def comparaison(self, liste: list) -> None:
-        """Additionne les cases si possible"""
-        for i in range(3):
-            depart, arrivee = liste[i], liste[i + 1]
-            if self[depart] != 0 and self[depart] == self[arrivee]:
-                self[depart] += self[depart]
-                self.score += self[depart]
-                self[arrivee] = 0
-                self += arrivee
-                if self.graphism:
-                    self.animation(depart, arrivee)
+    def moving(self, start: int, end: int, list_: list) -> None:
+        """Move zeroes and numbers"""
+        next_ = start + 1
+        if next_ > 3:
+            return
+        lnext, lstart = list_[next_], list_[start]
+        snext, sstart = self[lnext], self[lstart]
+        if snext != 0:
+            self.moving(next_, end, list_)
+        elif snext == sstart:
+            self.moving(next_, end, list_)
+        else:
+            self[lstart], self[lnext] = (snext, sstart)
+            self -= lnext
+            self += lstart
+            if start > 0 and self[list_[start - 1]] != 0:
+                self.moving(start - 1, end, list_)
+            else:
+                self.moving(next_, end, list_)
 
-    def animation(self, arrivee: int, depart: int, debug=False) -> None:
-        """
-        Met a jour les elements lors d'une action
-        (generalement 'up','down','left','right')
-        Si 'debug' vaut False, une animation de deplacement des nombres est permise
-        """
-        if not (debug):
-            self.interface.animation(self[arrivee], arrivee, arrivee - depart)
-        self.update(depart)
-        self.update(arrivee)
+    def compare(self, order: list) -> None:
+        """Add les cells if possible"""
+        for list_ in order:
+            for i in range(3):
+                start, end = list_[i], list_[i + 1]
+                if self[start] != 0 and self[start] == self[end]:
+                    self[start] += self[start]
+                    self.score += self[start]
+                    self[end] = 0
+                    self += end
+
+    def before(self) -> None:
+        """Only if there is an `interface`. Set the `grid` for the previous state"""
+        if self.graphism:
+            self.interface.setbefore(self.grid)
+
+    def animation(self, key: int) -> None:
+        """Update elements during an action `up`, `down`, `left`, `right`"""
+        if self.graphism:
+            self.interface.setafter(self.grid)
+            self.interface.animation(self.ORDERS[key])
 
     def update(self, position: int) -> None:
-        """Met a jour l'objet de la grille à la position donnee"""
+        """Update the object on grid according to `position`"""
         self.interface.blit(self[position], position)
         self.interface.update(position)
 
+    def update_all(self):
+        for i in range(16):
+            self.update(i)
+
     def random(self) -> None:
-        """Genere un nombre (2 ou 4) et le met a jour graphiquement"""
+        """Choose randomly a number (2 ou 4) and update graphicaly"""
         r = choice(self.zero)
-        self.grille[r] = self.random_2_4()
+        self.grid[r] = self.random_2_4()
         self -= r
         if self.graphism:
             self.update(r)
 
-    def up(self, rd: bool = True) -> bool:
-        """Mouvement haut"""
-        if self.partial_move(-4):
-            for i in range(4):
-                self.move_zero(self.INV_COLONNE[i])
-            for i in range(4):
-                self.comparaison(self.COLONNE[i])
-            for i in range(4):
-                self.move_zero(self.INV_COLONNE[i])
-
-            if rd:
-                self.random()
-            return True
-
-    def down(self, rd: bool = True) -> bool:
-        """Mouvement bas"""
-        if self.partial_move(4):
-            for i in range(4):
-                self.move_zero(self.COLONNE[i])
-            for i in range(4):
-                self.comparaison(self.INV_COLONNE[i])
-            for i in range(4):
-                self.move_zero(self.COLONNE[i])
-
-            if rd:
-                self.random()
-            return True
-
-    def right(self, rd: bool = True) -> bool:
-        """Mouvement droite"""
-        if self.partial_move(1):
-            for i in range(4):
-                self.move_zero(self.LIGNE[i])
-            for i in range(4):
-                self.comparaison(self.INV_LIGNE[i])
-            for i in range(4):
-                self.move_zero(self.LIGNE[i])
-
-            if rd:
-                self.random()
-            return True
-
-    def left(self, rd: bool = True) -> bool:
-        """Mouvement gauche"""
-        if self.partial_move(-1):
-            for i in range(4):
-                self.move_zero(self.INV_LIGNE[i])
-            for i in range(4):
-                self.comparaison(self.LIGNE[i])
-            for i in range(4):
-                self.move_zero(self.INV_LIGNE[i])
-
+    def action(self, key, rd: bool = True):
+        if self.partial_move(key):
+            self.before()
+            self.move_zero(self.ORDERS[key])
+            self.compare(self.ORDERS[-key])
+            self.move_zero(self.ORDERS[key])
+            self.animation(key)
             if rd:
                 self.random()
             return True
 
     def move(self) -> bool:
-        """Verifie si l'on peut continuer à jouer"""
+        """Check if the player can play"""
         if len(self.zero) != 0:
             return True
         i = 0
         condition = False
-        bordure_droite = [3, 7, 11]
-        bordure_bas = [12, 13, 14]
+        right_border = [3, 7, 11]
+        bottom_border = [12, 13, 14]
         while i < 15 and not (condition):
-            if i in bordure_droite:
+            if i in right_border:
                 if self[i] == self[i + 4]:
                     condition = True
                 else:
                     i += 1
-            elif i in bordure_bas:
+            elif i in bottom_border:
                 if self[i] == self[i + 1]:
                     condition = True
                 else:
@@ -278,12 +222,12 @@ class Game2048:
                 i += 1
         return condition
 
-    def movement_condition(self, l: list, n: int) -> callable:
-        """Genere une fonction condition utile pour la fonction 'partial_move'"""
+    def condition(self, l: list, n: int) -> callable:
+        """Generate a function useful for 'partial_move'"""
         return lambda i: not (i in l) and (self[i] == self[i + n] or self[i + n] == 0)
 
     def partial_move(self, movement: int) -> bool:
-        """Verifie si le mouvement est jouable"""
+        """Check if the movement is playable"""
         liste = [i for i in range(16) if not (i in self.zero)]
         condition = False
         j = 0
@@ -299,19 +243,10 @@ class Game2048:
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            self.up()
-            pygame.time.delay(self.time)
+            self.action(-4)
         elif keys[pygame.K_DOWN]:
-            self.down()
-            pygame.time.delay(self.time)
+            self.action(4)
         elif keys[pygame.K_LEFT]:
-            self.left()
-            pygame.time.delay(self.time)
+            self.action(-1)
         elif keys[pygame.K_RIGHT]:
-            self.right()
-            pygame.time.delay(self.time)
-
-
-if __name__ == "__main__":
-    game = Game2048()
-    game.start()
+            self.action(1)
